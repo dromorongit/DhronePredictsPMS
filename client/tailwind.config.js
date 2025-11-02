bf3c4e88384a9b890eacdc425e869f0d74cb74b0
Narh Henry Padi Dromor <hpdnarh@gmail.com>
2025-11-02 23:57:59 +0000

    New Commits

---
+++ client/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#0033cc',
+        'secondary': '#ffcc00',
+      },
+      fontFamily: {
+        sans: ['-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', '"Fira Sans"', '"Droid Sans"', '"Helvetica Neue"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
